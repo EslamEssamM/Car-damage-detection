b2b81543d494512707af9d819736cd9ba2ce746c
EslamEssamM <ee3@pme.suezuni.edu.eg>
2024-11-30 20:09:24 +0200

    Update main.tsx to display service hold message and comment out RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -16,20 +16,21 @@ const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 // Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement);
-	root.render(
-		<StrictMode>
-			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-				<RouterProvider router={router} />
-			</ThemeProvider>
-		</StrictMode>,
-	);
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1>Service on hold الخدمة متوقفة</h1>
+        {/* <RouterProvider router={router} /> */}
+      </ThemeProvider>
+    </StrictMode>
+  );
 }
